cfd2c73cf039ad63ce6e6e029d8e944c799f5e6c
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-02 15:29:27 +0800

    Update logomaker.py

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -114,7 +114,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Zebo Robot](https://t.me/ZeboRobot)",
+            caption="Logo by [ᴢᴇʙᴏ](https://t.me/Zeamusicuser_bot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -198,7 +198,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Zeabo Robot](https://t.me/ZeboRobot)",
+            caption="Logo by [ᴢᴇʙᴏ](https://t.me/zeamusicuser_bot)",
             force_document=False,
         )
         os.remove(flnme)
