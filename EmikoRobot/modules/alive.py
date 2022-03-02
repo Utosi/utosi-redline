020e6b5b1dc3c9ab4f0e8735e5affe33ed09f803
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-02 15:23:00 +0800

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -14,7 +14,7 @@ PHOTO = "https://telegra.ph/file/43d1fd4fa4043789c66c2.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Zebo.** \n\n"
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ᴢᴇʙᴏ.** \n\n"
     TEXT += "⚪ **I'm Working Properly** \n\n"
     TEXT += f"⚪ **My Master : [ZeaFeya](https://t.me/zeafeya)** \n\n"
     TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
