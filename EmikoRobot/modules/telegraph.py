ea65e50ababaa9592527d033511e3d562399c243
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-02 15:26:40 +0800

    Update telegraph.py

--- EmikoRobot/modules/telegraph.py
+++ EmikoRobot/modules/telegraph.py
@@ -11,7 +11,7 @@ from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 
-wibu = "EmikoRobot"
+wibu = "ᴢᴇʙᴏ"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=wibu)
 auth_url = r["auth_url"]
